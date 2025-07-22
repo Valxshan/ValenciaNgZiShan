aacb568fa0590de788b13cc43ddfe57bddb8f2dc
Valxshan <valenciangzishan@gmail.com>
2025-07-22 21:55:56 +0800

    Update question2.js

--- Assignment3/question2.js
+++ Assignment3/question2.js
@@ -1,7 +1,7 @@
-const msftData2 = [190.15, 194.21, 191.07, 190.44, 200.15];
+const msftData = [190.15, 194.21, 191.07, 190.44, 200.15];
 
 // Write your code below
 //You are to add in the following missing data: 205.67
 
-msftData2.push(205.67);
-console.log(msftData2);
+msftData.push(205.67);
+console.log(msftData);
